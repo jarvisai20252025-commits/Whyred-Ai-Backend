0a24c0359caa3873b4cd3c4b33d2461a4a0e4003
jarvis <jarvisai20252025@gmail.com>
2025-10-25 17:38:56 +0530

    Add root route and fix Cannot GET /

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -9,8 +9,14 @@ def init_firebase():
         service_account_info = {
             "type": "service_account",
             "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": "1",
             "private_key": os.getenv("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
             "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": "",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.getenv('FIREBASE_CLIENT_EMAIL')}"
         }
         
         cred = credentials.Certificate(service_account_info)
